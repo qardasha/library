5246d17a414cd656a1cd16e00dea1e5ad7b8ba1f
qardasha <107770203+qardasha@users.noreply.github.com>
2022-08-28 17:29:23 +1000

    Update func.js

--- func.js
+++ func.js
@@ -1,126 +1,232 @@
 let books = [
-    "Tafsir Al Qurtubi # Al Qurtubi # Aisha Bewley # English # Islam, Tafseer # https://ia801906.us.archive.org/30/items/TafsirAlQurtubiVolI/Tafsir%20al%20Qurtubi%20-%20Vol%20I.pdf # https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTwulnwAZnhoUaz9dyXEpqyf7VKF7GfXDt1Wg&usqp=CAU",
-    "Hamlet # William Shakespeare # N/A # English # Play, Tragedy, English # https://www.w3.org/People/maxf/XSLideMaker/hamlet.pdf # https://storage.googleapis.com/media365-live.appspot.com/b4f85bf3-c5ba-4c52-a2f6-452b1b157b8d/fa8c7171-ac02-4537-9eb4-9d9c474743bb/49424cf2-f58a-4560-8c0e-133472c24288.jpg",
-    "The Three Fundamental Principles # Muhammad Ibn Sulayman At Tameemee, Aboo Shaybah # N/A # English, Arabic # Islam # https://qaryah.files.wordpress.com/2011/11/class_3usool.pdf # https://www.sifatusafwa.com/15486/cart_default.jpg",
-    "The Art of War # Sun Tzu # Lionel Giles # English # War, Strategy # https://sites.ualberta.ca/~enoch/Readings/The_Art_Of_War.pdf # https://image.isu.pub/080216141343-d5dfbb0651434e4881dea5ca3941d7be/jpg/page_1_thumb_large.jpg",
-    "El Corán # N/A # Julio Cortés # Spanish # Islam, Quran # https://ia800103.us.archive.org/19/items/quran-trans/spanish.pdf # https://elpetirrojoec.com/wp-content/uploads/2021/06/312355_IMAGEN_TAPA_G_001.jpg",
-    "Sahih Al Bukhari # Imam Al Bukhari, Muhammad Muhsin Khan # N/A # English, Arabic # Islam, Hadeeth # https://www.muslim-library.com/dl/books/English_The_Translation_of_the_Meanings_of_Summarized_Sahih_Al_Bukhari.pdf # https://archive.org/services/img/SummarizedSahihAl-bukhari.pdf/full/pct:200/0/default.jpg",
-    "The War of Art # Steven Pressfield # N/A # English # Misc. # https://aimeeknight.files.wordpress.com/2016/01/the-war-of-art_fastpencil_pbo.pdf # https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1384802951l/18620536.jpg",
-    "A Summary of Islamic Jurisprudence (Volume 1) # Dr. Salih Al-Fawzan # Dar Al Asimah # English # Islam # https://darpdfs.org/wp-content/uploads/2020/04/A-Summary-Of-Islamic-Jurisprudence-Vol.-1-Sh.-Salih-Al-Fawzan-compressed.pdf # https://cdn.discordapp.com/attachments/1006384677768138865/1006890600878059630/unknown.png",
-    "A Summary of Islamic Jurisprudence (Volume 2) # Dr. Salih Al-Fawzan # Dar Al Asimah # English # Islam # https://ia902904.us.archive.org/34/items/ASummaryOfIslamicJurisprudence/En_a_summary_of_islamic_jurisprudence_part2.pdf # https://cdn.discordapp.com/attachments/1006384677768138865/1006892456274579456/unknown.png",
-    "The Boy and the Demon # Sultan Orzan bin Muhammad # N/A # English # Folk # https://docdro.id/5Mbbu19 # https://cdn.discordapp.com/attachments/1006384677768138865/1006895085956382790/Screen_Shot_2022-08-10_at_7.01.29_AM.png",
-    "Confirmed Jannati Hai # Sultan Orzan bin Muhammad # N/A # English # Folk, Islam # https://docdro.id/bTLfv42 # https://cdn.discordapp.com/attachments/1006384677768138865/1006895470204964864/unknown.png",
-    "Riyad As Saliheen # Imam An Nawawi # Dar Al Manarah # English # Islamic, Hadeeth # https://masjidtaqwa.org/pdfs/Riyad-us-Saliheen.pdf # http://cdn.shopify.com/s/files/1/0075/3290/4561/products/Riyad_Us_Saliheen__2_Vol.__dsbooks_australia_darussalam_islamicbookstore.jpg?v=1619087522",
-    "القرآن الكريم # N/A # N/A # Arabic # Islam, Quran # http://islamicbook.ws/quran/quran.pdf # https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1547257935l/25766113._SX318_.jpg",
-    "1984 # George Orwell # N/A # English # English # https://rauterberg.employee.id.tue.nl/lecturenotes/DDM110%20CAS/Orwell-1949%201984.pdf # https://i0.wp.com/www.printmag.com/wp-content/uploads/2017/01/2a34d8_a6741e88335241308890543d203ad89dmv2-1.jpg?resize=500%2C815&ssl=1",
-    "Macbeth # William Shakespeare # N/A # English # Play, Tragedy, English # https://thesefantasticworlds.com/wp-content/uploads/2016/04/Tragedy-of-Macbeth.pdf # https://www.insightpublications.com.au/wp-content/uploads/2018/07/ISP_Macbeth_cover_front.jpg",
+  "#t Tafsir Al Qurtubi #a Al Qurtubi #tr Aisha Bewley #lg English #cg Islam, Tafseer #u https://ia801906.us.archive.org/30/items/TafsirAlQurtubiVolI/Tafsir%20al%20Qurtubi%20-%20Vol%20I.pdf #bc https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTwulnwAZnhoUaz9dyXEpqyf7VKF7GfXDt1Wg&usqp=CAU",
+
+  "#t Hamlet #a William Shakespeare #lg English #tt Play #cg Tragedy, English Literature #u https://www.w3.org/People/maxf/XSLideMaker/hamlet.pdf #bc https://storage.googleapis.com/media365-live.appspot.com/b4f85bf3-c5ba-4c52-a2f6-452b1b157b8d/fa8c7171-ac02-4537-9eb4-9d9c474743bb/49424cf2-f58a-4560-8c0e-133472c24288.jpg",
+
+  "#t The Three Fundamental Principles #a Muhammad Ibn Sulayman At Tameemee #tr Aboo Shaybah #lg English, Arabic #cg Islam #u https://qaryah.files.wordpress.com/2011/11/class_3usool.pdf #bc https://www.sifatusafwa.com/15486/cart_default.jpg",
+
+  "#t The Art of War #a Sun Tzu #tr Lionel Giles #lg English #cg War, Strategy #u https://sites.ualberta.ca/~enoch/Readings/The_Art_Of_War.pdf #bc https://image.isu.pub/080216141343-d5dfbb0651434e4881dea5ca3941d7be/jpg/page_1_thumb_large.jpg",
+
+  "#t El Corán #tr Julio Cortés #lg Spanish #cg Islam, Quran #u https://ia800103.us.archive.org/19/items/quran-trans/spanish.pdf #bc https://elpetirrojoec.com/wp-content/uploads/2021/06/312355_IMAGEN_TAPA_G_001.jpg",
+
+  "#t Sahih Al Bukhari #ot صحيح البخاري #a Imam Al Bukhari #tr Muhammad Muhsin Khan #lg English, Arabic #cg Islam, Hadeeth #u https://www.muslim-library.com/dl/books/English_The_Translation_of_the_Meanings_of_Summarized_Sahih_Al_Bukhari.pdf #bc https://archive.org/services/img/SummarizedSahihAl-bukhari.pdf/full/pct:200/0/default.jpg",
+
+  "#t The War of Art #a Steven Pressfield #lg English #u https://aimeeknight.files.wordpress.com/2016/01/the-war-of-art_fastpencil_pbo.pdf #bc https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1384802951l/18620536.jpg",
+
+  "#t A Summary of Islamic Jurisprudence (Volume 1) #a Dr. Salih Al-Fawzan #tr Dar Al Asimah #lg English #olg Arabic #cg Islam #u https://darpdfs.org/wp-content/uploads/2020/04/A-Summary-Of-Islamic-Jurisprudence-Vol.-1-Sh.-Salih-Al-Fawzan-compressed.pdf #bc https://cdn.discordapp.com/attachments/1006384677768138865/1006890600878059630/unknown.png",
+
+  "#t A Summary of Islamic Jurisprudence (Volume 2) #a Dr. Salih Al-Fawzan #tr Dar Al Asimah #lg English #olg Arabic #cg Islam #u https://ia902904.us.archive.org/34/items/ASummaryOfIslamicJurisprudence/En_a_summary_of_islamic_jurisprudence_part2.pdf #bc https://cdn.discordapp.com/attachments/1006384677768138865/1006892456274579456/unknown.png",
+
+  "#t The Boy and the Demon #a Sultan Orzan bin Muhammad #lg English #tt Short Story #cg Folk #u https://docdro.id/5Mbbu19 #bc https://cdn.discordapp.com/attachments/1006384677768138865/1006895085956382790/Screen_Shot_2022-08-10_at_7.01.29_AM.png",
+
+  "#t Confirmed Jannati Hai #a Sultan Orzan bin Muhammad #lg English #cg Folk, Islam #tt Short Story #u https://docdro.id/bTLfv42 #bc https://cdn.discordapp.com/attachments/1006384677768138865/1006895470204964864/unknown.png",
+
+  "#t Riyad As Saliheen #a Imam An Nawawi #tr Dar Al Manarah #lg English, Arabic #cg Islamic, Hadeeth #u https://masjidtaqwa.org/pdfs/Riyad-us-Saliheen.pdf #bc http://cdn.shopify.com/s/files/1/0075/3290/4561/products/Riyad_Us_Saliheen__2_Vol.__dsbooks_australia_darussalam_islamicbookstore.jpg?v=1619087522",
+
+  "#t Al Qurān Al Kareem #ot القرآن الكريم #lg Arabic #tt Scripture #cg Islam, Quran, Religion #u http://islamicbook.ws/quran/quran.pdf #bc https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1547257935l/25766113._SX318_.jpg",
+
+  "#t 1984 #a George Orwell #lg English #tt Novel #cg English literature, Dystopia #u https://rauterberg.employee.id.tue.nl/lecturenotes/DDM110%20CAS/Orwell-1949%201984.pdf #bc https://i0.wp.com/www.printmag.com/wp-content/uploads/2017/01/2a34d8_a6741e88335241308890543d203ad89dmv2-1.jpg?resize=500%2C815&ssl=1",
+
+  "#t Macbeth #a William Shakespeare #lg English #tt Play #cg English literature, Tragedy #u https://thesefantasticworlds.com/wp-content/uploads/2016/04/Tragedy-of-Macbeth.pdf #bc https://www.insightpublications.com.au/wp-content/uploads/2018/07/ISP_Macbeth_cover_front.jpg",
+
+  "#t how to play chess #a michael crowe #pb scholastic chess ireland #lg english #u https://www.buffalolib.org/sites/default/files/gaming-unplugged/inst/1%20Chess%20Game%20Instructions.pdf #cg chess #tt guide #bc https://cdn.discordapp.com/attachments/1006384677768138865/1012565288945975306/IMG_0994.png",
+
+  "#t chess : the complete guide to chess - master chess tactics, chess openings and chess strategies (4th edition) #a logan donovan #cg chess #tt guide #u dspace.vnbrims.org:13000/jspui/bitstream/123456789/4980/1/Chess_%20The%20Complete%20Guide%20To%20Chess%20-%20Master_%20Chess%20Tactics%2C%20Chess%20Openings%2C%20and%20Chess%20Strategies.pdf #bc https://cdn.discordapp.com/attachments/1006384677768138865/1012565661706362981/IMG_0995.png",
+
+  "#t the qur’an #tr saheeh international #lg english #olg arabic #cg islam, quran #tt scripture #u https://asimiqbal2nd.files.wordpress.com/2009/06/quran-sahih-international.pdf #bc https://cdn.discordapp.com/attachments/1006384677768138865/1012566933163483216/569B33B1-EF2C-44EA-904B-02BABF7AEEE3.jpg",
+
+  "#t matnu-shātibiyyah al-musamma hirz-ul-amāni wa-wajhu-t-tahāni fil qirā’āti-s-sab’ #ot متن الشاطبية المسمى حرز الأماني ووجه التهاني في القراءات السبع #lg arabic #a Imam Ash-Shatibi #u https://d1.islamhouse.com/data/ar/ih_books/single5/ar_matan_alshatbiah.pdf #bc https://cdn.discordapp.com/attachments/1006384677768138865/1012569958611423322/553F9468-255E-4362-ACEE-7ED7244F2267.jpg",
+
+  "#t the history of herodotus: volume 1 #a herodotus #tr g.c. macaulay #lg english #pb macmillan and co #the histories #olg greek #bc #u https://wps.pearsoncustom.com/wps/media/objects/2426/2484749/chap_assets/bookshelf/herodotus.pdf #tt history #cg history #bc https://cdn.discordapp.com/attachments/1006384677768138865/1013111578138263612/unknown.png",
+
+  "#t Mathematics for the twelfth grade I (second edition) - Arabic #a Omran Kouba #lg Arabic #cg Mathematics #u https://www.docdroid.net/DZuXRLJ/g12-sb1-pdf #bc https://cdn.discordapp.com/attachments/1006384677768138865/1010030425961398353/unknown.png"
 ]
+
+let bookCountHeader = document.getElementById("book-count-header");
+
+let infoPopup = document.getElementById("info-popup");
+
+bookCountHeader.innerHTML = `Search from a catalogue of ${books.length} books`
 
 let bookshelf = document.querySelector(".bookshelf");
 
-function createBook() {
-    books.forEach(book => {
-        let info = book.split("#");
+function createBooks() {
+  books.forEach(book => {
+    let parsedRecord = book.split("#")
+    parsedRecord.shift()
 
-        bookshelf.innerHTML +=
-        `<button class="book-box" onclick="openFile('${info[5].trim()}')">
-            <div class="left-side">
-                <img class="book-cover" src="${info[6]}" alt="">
-            </div>
-            <div class="right-side">
-                <div class="book-title-authors">
-                    <p class="book-title">${info[0]}</p>
-                    ${checkAuthors(info[1])}
-                </div>
-                ${checkTranslators(info[2])}
-                <div class="languages-div">
-                    ${checkLanguages(info[3])}
-                </div>
-                <div class="categories-div">
-                    ${checkCategories(info[4])}
-                </div>
-            </div>
-        </button>`
-    });
+    let t
+    let a
+    let bc
+
+    for (let i = 0; i < parsedRecord.length; i++) {
+      let pr = parsedRecord[i].split(/ (.*)/)
+
+      switch (pr[0]) {
+        case "t":
+          let newData = [];
+          let splitT = pr[1].split(" ");
+          splitT.forEach(d => {
+            d = d.charAt(0).toUpperCase() + d.slice(1);
+            newData.push(d)
+          });
+          t = newData.join(" ");
+          break
+        case "a":
+          let newDataA = [];
+          let splitTA = pr[1].split(" ");
+          splitTA.forEach(d => {
+            d = d.charAt(0).toUpperCase() + d.slice(1);
+            newDataA.push(d)
+          });
+          a = newDataA.join(" ");
+          break
+        case "bc":
+          bc = pr[1]
+      }
+    }
+
+    bookshelf.innerHTML +=
+      `
+			<div class="book-box" data-info="${book}">
+				<div class="image-div">
+					<img src="${bc}">
+				</div>
+		
+				<div class="info-div">
+					<p class="title-info">${t}</p>
+					${checkAuthors(a)}
+				</div>
+				
+				<button onclick="displayInfo(event)">Info</button>
+			</div>
+			`
+  });
 }
 
-createBook();
+createBooks();
 
 function checkAuthors(info) {
-    if (info !== " N/A ") return `<p class="book-authors">${info}</p>`;
-    else return "";
+  if (info !== undefined) return `<p>${info}</p>`;
+  else return "";
 }
 
-function checkTranslators(info) {
-    if (info !== " N/A ") return `<p class="book-translators">Translated by ${info}</p>`;
-    else return "";
+document.onclick = function(e) {
+  if ((e.target.id !== infoPopup.id || e.target.parentElement.id !== infoPopup.id) && !infoPopup.classList.contains("invisible")) {
+    //infoPopup.classList.add("invisible");
+    //console.log("hello")
+  }
 }
 
-function checkLanguages(info) {
-    let parts = info.split(",");
-    
-    if (parts.length > 1) {
-        let partsToReturn = "";
-
-        parts.forEach(part => {
-            partsToReturn += `<div class="language-button">${part}</div>`
-        });
-
-        return partsToReturn;
+function displayInfo(e) {
+  Array.from(infoPopup.children).forEach(info => {
+    info.innerHTML = "";
+    if (info.classList.contains("u")) {
+      info.innerHTML = "Read"
     }
-    else {
-        return `<div class="language-button">${parts}</div>`
+  });
+
+  let data = e.currentTarget.parentElement.dataset.info
+  infoPopup.classList.remove("invisible")
+
+  let parsedRecord = data.split("#")
+  parsedRecord.shift()
+
+  for (let i = 0; i < parsedRecord.length; i++) {
+    let pr = parsedRecord[i].split(/ (.*)/)
+
+    switch (pr[0]) {
+      case "t":
+        createInfo("Title", pr[0], pr[1])
+        break
+      case "a":
+        createInfo("Author", pr[0], pr[1])
+        break
+      case "il":
+        createInfo("Illustrator", pr[0], pr[1])
+        break
+      case "lg":
+        createInfo("Language", pr[0], pr[1])
+        break
+      case "tr":
+        createInfo("Translator", pr[0], pr[1])
+        break
+      case "pb":
+        createInfo("Publisher", pr[0], pr[1])
+        break
+      case "ot":
+        createInfo("Original Title", pr[0], pr[1])
+        break
+      case "olg":
+        createInfo("Original Language", pr[0], pr[1])
+        break
+      case "tt":
+        createInfo("Text type", pr[0], pr[1])
+        break
+      case "cg":
+        createInfo("Category", pr[0], pr[1])
+        break
+      case "bc":
+        addPhoto(pr[0], pr[1])
+        break
+      case "u":
+        addLink(pr[0], pr[1])
+        break
     }
+  }
 }
 
-function checkCategories(info) {
-    let partsUntrimmed = info.split(",");
-    let parts = partsUntrimmed.map(part => {
-        return part.trim();
-    });
-    if (parts.length > 1) {
-        let partsToReturn = "";
-
-        parts.forEach(part => {
-            partsToReturn += `<a href="#" class="category-button">#${part}</a>`
-        });
-
-        return partsToReturn;
-    }
-    else if (parts.length == 1) {
-        return `<a href="#" class="category-button">#${parts}</a>`
-    }
+function createInfo(title, letter, data) {
+  let newData = [];
+  let letterDiv = infoPopup.querySelector(`.${letter}`)
+  let span = document.createElement("span")
+  span.innerHTML = title + ": "
+  let splitData = data.split(" ");
+  splitData.forEach(d => {
+    d = d.charAt(0).toUpperCase() + d.slice(1);
+    newData.push(d);
+  });
+  letterDiv.innerHTML = span.outerHTML + newData.join(" ");
 }
 
-function openFile(file) {
-    let url = window.location.toString().split("library")[0] + `/library/pdf-viewer.html#${file}`;
-    window.open(url, "_blank");
+function addPhoto(letter, data) {
+  let letterDiv = infoPopup.querySelector(`.${letter}`)
+  letterDiv.src = data
 }
 
-let noBooksAvailable = document.getElementById("no-books-available");
-
-function setUpdateBookViewTimeout(href)
-{
-    setTimeout( function() { updateBookView(href); }, 1 );
+function addLink(letter, data) {
+  let letterDiv = infoPopup.querySelector(`.${letter}`);
+  letterDiv.value = data;
+  letterDiv.addEventListener("click", openFile);
 }
 
-function updateBookView(href) {
-    let category = window.location.toString().split("#")[1];
+function makeInvisible() {
+  infoPopup.classList.add("invisible");
+}
 
-    for (let i = 0; i < bookshelf.children.length; i++) {
-        let book = bookshelf.children[i];
+document.onkeyup = function(e) {
+  if (e.keyCode == 81) {
+    infoPopup.classList.add("invisible");
+  }
+}
 
-        let categoriesText = book.querySelector(".categories-div").innerHTML.toLowerCase();
+function openFile(event) {
+  let file = event.currentTarget.value;
+  let url = window.location;
+  if (url.toString().includes("index.html")) url = url.toString().replace("index.html", `file-viewer.html#${file}`);
+  else url = url.toString().concat(`file-viewer.html#${file}`);
+  window.open(url, "_blank");
+}
 
-        console.log(category, categoriesText);
-        if (categoriesText.includes(category)) {
-            book.classList.remove("hide-category");
+function check(input) {
+    let splitInput = input.value.split(", ");
+    Array.from(bookshelf.children).forEach(book => {
+        for (let i = 0; i < splitInput.length; i++) {
+            if (!book.dataset.info.toLowerCase().includes(splitInput[i].toLowerCase())) {
+                book.style.display = "none";
+                break;
+            }
+            else book.style.display = "flex";
         }
-        else book.classList.add("hide-category");
-    }
-
-    if (bookshelf.querySelectorAll(".hide-category").length == bookshelf.children.length) {
-        noBooksAvailable.style.display = "block";
-    }
-    else noBooksAvailable.style.display = "none";
+    });
 }
